f9225a5a086654ceef70406d9dbbf09449ebbd13
Elaine Cadman <elaine.cadman@gmail.com>
2018-10-06 22:59:47 -0700

    hw submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,6 @@
-var img = document.createElement("img");
- 
-img.src = "https://media-cdn.tripadvisor.com/media/photo-s/12/f5/f1/8d/eiffel-tower-summit-priority.jpg";
-var src = document.getElementById("eiffel");
-
-
-
 // Creating variabless to hold stats
-var guessesRemaining = 12;
+var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guessesRemaining = 5;
 var wins = 0;
 var losses = 0;
 var pastGuesses = "";
@@ -18,7 +12,7 @@ var guessBank = [];
 var directionsText = document.getElementById("directions-text");
 var computerWordText = document.getElementById("computerword-text");
 var userChoiceText = document.getElementById("userchoice-text");
-var guessesRemainingText  = document.getElementById("guessesRemaining-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var pastGuessesText = document.getElementById("pastGuesses-text");
@@ -39,78 +33,82 @@ document.onkeyup = event => {
     // user guesses by hitting key
     var userGuess = event.key;
 
-    if(guessBank.includes(!userGuess)){
-    pastGuesses=pastGuesses + event.key + ", ";
+    if (userChoices.includes(userGuess)) {
+        if (guessBank.includes(!userGuess)) {
+            pastGuesses = pastGuesses + event.key + ", ";
+        }
+        // if(guessBank.includes(userGuess)){
+        //     pastGuesses=pastGuesses;
+        // }
+
+        //if the user Guess is not in the word, guesses go down by 1
+        if ((!chosenWord.includes(userGuess)) && (!guessBank.includes(userGuess))) {
+            guessesRemaining--;
+        }
+
+        if (guessesRemaining === 0) {
+            losses++;
+            alert("Awww...that one was hard...you lose.")
+            guessBank = [];
+            guessesRemaining = 5;
+            chosenWord = computerWord[Math.floor(Math.random() * computerWord.length)];
+        }
+
+
+        //Loop for finding correct characters
+        for (i = 0; i < chosenWord.length; i++) {
+
+            if (((chosenWord.charAt(i)) === userGuess) || (guessBank.includes(chosenWord.charAt(i)))) {
+                guessBank.push(userGuess);
+                answerArray[i] = chosenWord[i];
+            } else if ((chosenWord.charAt(i)) !== userGuess) {
+                guessBank.push(userGuess);
+                answerArray[i] = "_"
+            }
+            if ((chosenWord.charAt(i)) === " ") {
+                answerArray[i] = " "
+            }
+
+        }
+
+        //choosenWordArray[i] = userGuess
+
+        var filteredArray = guessBank.filter(function (item, pos) {
+            return guessBank.indexOf(item) == pos;
+        })
+
+        console.log("filtered array" + filteredArray);
+
+
+
+        console.log("chosen word: " + chosenWord);
+        console.log("user guess: " + userGuess);
+
+
+
+        if (!answerArray.includes("_")) {
+            wins++;
+            alert("Wow! You're super smart! You win!");
+            guessesRemaining = 5;
+            guessBank = [];
+            chosenWord = computerWord[Math.floor(Math.random() * computerWord.length)];
+
+        }
+
+
+        // Display the user stats.
+
+        userChoiceText.textContent = "You chose: " + userGuess;
+        // chosenWordText.textContent = "The computer chose: " + chosenWord;
+        pastGuessesText.textContent = "Your previous guesses: " + filteredArray;
+        // displayWordText.textContent = "Answer: " + answerArray;
+        guessesRemainingText.textContent = "Your guesses remaining: " + guessesRemaining;
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        // hiddenWordText.textContent = (spaces);
+        // splitWordText.textContent = "split word: " + splitWord;
+        test2Text.innerHTML = "Word You're guessing: " + answerArray;
+
+
     }
-    // if(guessBank.includes(userGuess)){
-    //     pastGuesses=pastGuesses;
-    // }
-    
-//if the user Guess is not in the word, guesses go down by 1
-    if((!chosenWord.includes(userGuess)) && (!guessBank.includes(userGuess))){
-        guessesRemaining--;
-    }
-
-    if(guessesRemaining === 0){
-        losses++;
-        alert("Awww...that one was hard...you lose.")
-    }
-   
-    
-
-    //Loop for finding correct characters
-for(i=0; i<chosenWord.length; i++){
-
-    if(((chosenWord.charAt(i)) === userGuess) || (guessBank.includes(chosenWord.charAt(i)))){
-        guessBank.push(userGuess);
-        answerArray[i] = chosenWord[i];
-    } 
-   
-    else if ((chosenWord.charAt(i)) !== userGuess){
-        guessBank.push(userGuess);
-        answerArray[i] = "_"
-    }
-    if ((chosenWord.charAt(i)) === " "){
-        answerArray[i] = " "
-    }
-   
-}
-
-    //choosenWordArray[i] = userGuess
-    
-    var filteredArray = guessBank.filter(function(item, pos){
-        return guessBank.indexOf(item)== pos; 
-      });
-      
-      console.log( "filtered array" + filteredArray );
-
-
-
-console.log("chosen word: " + chosenWord);
-console.log("user guess: " + userGuess);
-
-
-
-if(!answerArray.includes("_")){
-    wins++;
-    alert("Wow! You're super smart! You win!");
-}
-
-
-
-// Display the user stats.
-
-userChoiceText.textContent = "You chose: " + userGuess;
-chosenWordText.textContent = "The computer chose: " + chosenWord;
-pastGuessesText.textContent = "Your previous guesses: " + filteredArray;
-// displayWordText.textContent = "Answer: " + answerArray;
-guessesRemainingText.textContent = "Your guesses remaining: " + guessesRemaining;
-winsText.textContent = "wins: " + wins;
-lossesText.textContent = "losses: " + losses;
-// hiddenWordText.textContent = (spaces);
-// splitWordText.textContent = "split word: " + splitWord;
-test2Text.innerHTML = "Word You're guessing: " + answerArray;
-
-
-
 };
---
+++ assets/javascript/game3.js
@@ -0,0 +1,12 @@
+function showEiffel() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "https://media-cdn.tripadvisor.com/media/photo-s/12/f5/f1/8d/eiffel-tower-summit-priority.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+}
+if((wins++) && (chosenWord==="eiffel tower")){
+    showEiffel();
+}
+
